55eba2706ffcda7df7fb979c76440d415d68ab04
karankumar89 <karankumar89@csu.fullerton.edu>
2015-03-11 21:24:47 -0700

    cleaning and adding jshint

--- Chapter5/apiSearch/search.js
+++ Chapter5/apiSearch/search.js
@@ -1,17 +1,20 @@
+// Client-side code
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */ 
 $(document).ready(function(){
+    "use strict";
     var searchKey ;
-    $('#submit').click(function(){
+    $("#submit").click(function(){
         //console.log("click");
-        $('#results').empty();
-        searchKey = $('#key').val();
-        var myApi = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' + searchKey + '&callback=searchTerm';
+        $("#results").empty();
+        searchKey = $("#key").val();
+        var myApi = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" + searchKey + "&callback=searchTerm";
         $(function () {
             $.ajax({
                 url: myApi,
                 type: "GET",
-                dataType: 'jsonp',
-                jsonp: 'searchTerm',
-                async: 'true',
+                dataType: "jsonp",
+                jsonp: "searchTerm",
+                async: "true",
                 success: function (data) {
                     alert("success");
                 }
@@ -21,11 +24,12 @@ $(document).ready(function(){
 });
 
 function searchTerm(data) {
+    "use strict";
     //console.log(data.responseData.results);
     $("#results").append("<ul class='list-group'>");
     $.each(data.responseData.results, function(i, rows) {
         //console.log(rows.title);
-        $('#results').append("<li class='list-group-item'>"+rows.title + "<br>" +"<a href='"+rows.url+"' >"+rows.url+"</a>"+"</br></li>");
+        $("#results").append("<li class='list-group-item'>"+rows.title + "<br>" +"<a href='"+rows.url+"' >"+rows.url+"</a>"+"</br></li>");
     });
-    $("#results").append('</ul>');
+    $("#results").append("</ul>");
 }
